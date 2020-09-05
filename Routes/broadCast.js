99b552b65673ee8e7b3296b303e6856a5dad19ad
Chikoom <callforcopy@gmail.com>
2020-09-05 08:15:43 +0300

    video start

--- Routes/broadCast.js
+++ Routes/broadCast.js
@@ -2,95 +2,133 @@ const express = require('express')
 const BroadcastRoom = require('../Schema/BroadcastRoom')
 const VideoStream = require('../Schema/VideoStream')
 const router = express.Router()
-const moment = require('moment');
-moment().format();
+const moment = require('moment')
+moment().format()
 
-
-
-let timeChecker = function (startTimeCorentShow) {
-    let nowTime = moment(new Date());
-    let duration = moment.duration(nowTime.diff(startTimeCorentShow))
-    const minutes = duration.asMinutes();
-    if (minutes > 30) {
-        return false
-    } else {
-        return true
-    }
+const timeChecker = function (startTimeCorentShow) {
+  let nowTime = moment(new Date())
+  console.log(nowTime)
+  let duration = moment.duration(nowTime.diff(startTimeCorentShow))
+  const minutes = duration.asMinutes()
+  if (minutes > 30) {
+    return false
+  } else {
+    return true
+  }
 }
 
+const checkParticipant = (room, userID) => {
+  console.log(room)
+  const { participants, creator } = room
+  const singleParticipant = participants.find(
+    participant => participant.userID === userID
+  )
+  const userIs = {
+    creator: parseInt(creator) === userID ? true : false,
+    participant: singleParticipant ? true : false,
+  }
+  return userIs
+}
+
+const startShow = roomID => {}
+
 router.get('/:showID', async function (req, res) {
-    let roomID = req.params.showID
-    let showInfo = req.body
-    const timeAfterCheck = timeChecker(showInfo.startTime)
-    BroadcastRoom.findOne({ roomID: roomID }, function (err, result) {
-        console.log(result)
-        if (result && timeAfterCheck) {
-            res.send(result)
-            console.log("the show can start")
-        } else if (result && !timeAfterCheck) {
-            res.send(result)
-            console.log("you can open the show 30 min before the show start , come back later")
-        } else {
-            res.send("we dont have this room , please try diffrent id or try later , for support go to matan")
-            console.log("we dont have this room , please try diffrent id or try later , for support go to matan")
-        }
-    })
+  // console.log(req.query)
+  const userID = req.query.ID
+  let roomID = req.params.showID
+  let showInfo = req.body
+  const timeAfterCheck = timeChecker(showInfo.startTime)
+  BroadcastRoom.findOne({ roomID: roomID }, function (err, result) {
+    // console.log(result)
+
+    if (!result) {
+      res.status(404).send({
+        error: 'room not found',
+        roomID: roomID,
+      })
+    } else {
+      const participant = checkParticipant(result, parseInt(userID))
+      console.log(participant)
+    }
+
+    if (result && timeAfterCheck) {
+      res.send(result)
+      console.log('the show can start')
+
+      startShow(roomID)
+    } else if (result && !timeAfterCheck) {
+      res.send({
+        error:
+          'we dont have this room , please try diffrent id or try later , for support go to matan',
+      })
+      console.log(
+        'you can open the show 30 min before the show start , come back later'
+      )
+    } else {
+      res.status(404).send({
+        error:
+          'we dont have this room , please try diffrent id or try later , for support go to matan',
+      })
+      console.log(
+        'we dont have this room , please try diffrent id or try later , for support go to matan'
+      )
+    }
+  })
 })
 
 router.post('/:showID', async function (req, res) {
-    //creator creat room ,sand creatorID , showID, startTime , endTime
-    //need to add moment for time presentation 
-    let roomID = req.params.showID
-    let showInfo = req.body
-    let startTime = moment(showInfo.startTime).format("YYYY-MM-DD HH:mm");
-    let endTime = moment(showInfo.endTime).format("YYYY-MM-DD HH:mm");
+  //creator creat room ,sand creatorID , showID, startTime , endTime
+  //need to add moment for time presentation
+  let roomID = req.params.showID
+  let showInfo = req.body
+  let startTime = moment(showInfo.startTime).format('YYYY-MM-DD HH:mm')
+  let endTime = moment(showInfo.endTime).format('YYYY-MM-DD HH:mm')
 
-    let broadCast = new BroadcastRoom({
-        roomID: roomID,
-        participants: [],
-        creator: showInfo.creator,
-        startTime: startTime,
-        endTime: endTime,
-        isLive: false,
-        mainVideo: null
-    })
-    broadCast.save().then((err, result) => {
-        if (err) res.send(err)
-        else res.send(result)
-    })
+  let broadCast = new BroadcastRoom({
+    roomID: roomID,
+    participants: [],
+    creator: showInfo.creator,
+    startTime: startTime,
+    endTime: endTime,
+    isLive: false,
+    mainVideo: null,
+  })
+  broadCast.save().then((err, result) => {
+    if (err) res.send(err)
+    else res.send(result)
+  })
 })
 
 router.put('/:showID', async function (req, res) {
-    //user book and unbook participants
-    let { showID } = req.params
-    let { userID } = req.body
-    let { isBook } = req.body
-    if (isBook) {
-        BroadcastRoom.updateOne({ roomID: showID }, { $push: { participants: { userID, VideoStream: null } } })
-            .then(function (err, bookShow) {
-                if (err) res.send(err)
-                else res.send(bookShow)
-            })
-    }
-    else {
-        BroadcastRoom.updateOne({ roomID: showID }, { $pull: { participants: { userID, VideoStream: null } } })
-            .then(function (err, unbookShow) {
-                if (err) res.send(err)
-                else res.send(unbookShow)
-            })
-    }
+  //user book and unbook participants
+  let { showID } = req.params
+  let { userID } = req.body
+  let { isBook } = req.body
+  if (isBook) {
+    BroadcastRoom.updateOne(
+      { roomID: showID },
+      { $push: { participants: { userID, VideoStream: null } } }
+    ).then(function (err, bookShow) {
+      if (err) res.send(err)
+      else res.send(bookShow)
+    })
+  } else {
+    BroadcastRoom.updateOne(
+      { roomID: showID },
+      { $pull: { participants: { userID, VideoStream: null } } }
+    ).then(function (err, unbookShow) {
+      if (err) res.send(err)
+      else res.send(unbookShow)
+    })
+  }
 })
 
 router.delete('/:showID', async function (req, res) {
-    const roomID = req.params.showID
-    BroadcastRoom.findOneAndDelete({ roomID }).then(function (err, deleteShow) {
-        if (err) res.send(err)
-        else res.send(deleteShow)
-    })
-
-
+  const roomID = req.params.showID
+  BroadcastRoom.findOneAndDelete({ roomID }).then(function (err, deleteShow) {
+    if (err) res.send(err)
+    else res.send(deleteShow)
+  })
 })
 
 module.exports = router
-
-
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+})
+
 const broadCast = require('./Routes/broadCast')
 const chatRoom = require('./Routes/chatRoom')
 const message = require('./Routes/message')
@@ -11,18 +19,51 @@ require('dotenv').config()
 
 const { DB_URL } = process.env
 console.log(DB_URL)
-mongoose.connect(DB_URL, {useNewUrlParser: true,useUnifiedTopology: true, useFindAndModify: false})
-
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use('/peerjs', peerServer)
+app.get('/broadcast-room/:room', (req, res) => {
+  //res.render('room', { roomID: req.params.room })
+  res.send('OK')
+})
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  )
   next()
+})
+
+io.on('connection', socket => {
+  console.log('Connection recieved')
+  socket.emit('FromAPI', 'HELLO!')
+
+  socket.on('join-room', (roomID, userID) => {
+    console.log('joined room!')
+    console.log('Socket room ID', roomID)
+    console.log('PEER user ID', userID)
+
+    socket.join(roomID)
+    socket.to(roomID).broadcast.emit('user-conncted', userID)
+
+    socket.on('message', messageObj => {
+      console.log(messageObj.user)
+      console.log(messageObj.msg)
+      io.in(roomID).emit('user-message', messageObj)
+    })
+  })
 })
 
 app.use('/broadCast', broadCast)
@@ -30,4 +71,4 @@ app.use('/broadCast', broadCast)
 // app.use('/message', message)
 // app.use('/videoStream', videoStream)
 
-app.listen(8080, () => console.log("server up and running on port 8080"))
+server.listen(8080, () => console.log('server up and running on port 8080'))
